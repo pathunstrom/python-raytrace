040634b27c86235eaa15ddb46a9af4aab7bac77d
Piper Thunstrom <pathunstrom@gmail.com>
2021-09-30 00:27:03 -0700

    Canvas can save to ppm format.

--- src/tracer/renderer.py
+++ src/tracer/renderer.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+from pathlib import Path
+from typing import Union
+
 from .tuples import Color
 
 
 class Canvas:
 
-    def __init__(self, width: int, height: int):
-        self.pixels: list[Color] = [Color(0, 0, 0) for _ in range(width * height)]
+    def __init__(self, width: int, height: int, default: Color = Color(0, 0, 0)):
+        self.pixels: list[Color] = [default for _ in range(width * height)]
         self.width: int = width
         self.height: int = height
 
@@ -18,6 +21,30 @@ class Canvas:
 
     def __setitem__(self, key: tuple[int, int], value: Color):
         self.pixels[self._coordinate_to_index(*key)] = value
+
+    def save(self, file_path: Union[Path, str]):
+        with open(file_path, "w") as ppm:
+            lines = ["P3\n", f"{self.width} {self.height}\n", "255\n"]
+            for offset in range(0, ((self.height - 1) * self.width) + 1, self.width):
+                line_pixels = []
+                line_pixels_total_characters = 0
+                for pixel in self.pixels[offset:offset + self.width]:
+                    for element in pixel:
+                        element = min(max(round(element * 255), 0), 255)
+                        element_string = str(element)
+                        element_length = len(element_string)
+                        if line_pixels_total_characters + element_length + len(line_pixels) > 70:
+                            line = " ".join(line_pixels)
+                            line += "\n"
+                            lines.append(line)
+                            line_pixels = []
+                            line_pixels_total_characters = 0
+                        line_pixels.append(element_string)
+                        line_pixels_total_characters += element_length
+                line = " ".join(line_pixels)
+                line += "\n"
+                lines.append(line)
+            ppm.writelines(lines)
 
     def _coordinate_to_index(self, x, y):
         return x + (y * self.width)
--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -16,3 +16,58 @@ def test_set_pixel():
     canvas = Canvas(10, 20)
     canvas[2, 3] = red
     assert canvas[(2, 3)] == red
+
+
+def test_save_canvas_headers(tmp_path):
+    file_path = tmp_path / "test_file.ppm"
+    canvas = Canvas(5, 3)
+    canvas.save(file_path)
+    with open(file_path) as ppm:
+        identifier, size, max_color = (l.strip() for l in ppm.readlines()[:3])
+        assert identifier == "P3"
+        assert size == "5 3"
+        assert max_color == "255"
+
+
+def test_save_canvas_pixels(tmp_path):
+    file_path = tmp_path / "test_file.ppm"
+    canvas = Canvas(5, 3)
+    color_1 = Color(1.5, 0, 0)
+    color_2 = Color(0, 0.5, 0)
+    color_3 = Color(-0.5, 0, 1)
+
+    canvas[0, 0] = color_1
+    canvas[2, 1] = color_2
+    canvas[4, 2] = color_3
+
+    canvas.save(file_path)
+
+    with open(file_path) as ppm:
+        pixel_data = ppm.readlines()[3:6]
+        expected = """255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+"""
+        assert "".join(pixel_data) == expected
+
+
+def test_splitting_long_lines_in_ppm(tmp_path):
+    test_file_path = tmp_path / "test_file.ppm"
+    canvas = Canvas(10, 2, default=Color(1, 0.8, 0.6))
+    canvas.save(test_file_path)
+    with open(test_file_path) as ppm:
+        pixel_data = "".join(ppm.readlines()[3:])
+        expected = """255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204
+153 255 204 153 255 204 153 255 204 153 255 204 153
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204
+153 255 204 153 255 204 153 255 204 153 255 204 153
+"""
+        assert pixel_data == expected
+
+
+def test_ends_with_new_line(tmp_path):
+    test_file_path = tmp_path / "test_file.ppm"
+    canvas = Canvas(5, 3)
+    canvas.save(test_file_path)
+    with open(test_file_path) as ppm:
+        assert ppm.readlines()[-1].endswith("\n")

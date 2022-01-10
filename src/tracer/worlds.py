86819f4ce867fa706b6c31d161b0f4dad4a38c3f
Piper Thunstrom <pathunstrom@gmail.com>
2022-01-09 21:40:18 -0800

    Adds a default world.

--- src/tracer/worlds.py
+++ src/tracer/worlds.py
@@ -1,12 +1,35 @@
-from .physicals import Light
+from __future__ import annotations
+from dataclasses import dataclass, field
+
+from .matrices import Matrix
+from .physicals import Light, Hull, Sphere, Material
 from .tuples import Tuple, Color
 
 
+@dataclass
 class World:
-
-    def __init__(self, *objs, lights=tuple()):
-        self.lights = list(lights)
-        self.children = list(objs)
+    children: list[Hull] = field(default_factory=list)
+    lights: list[lights] = field(default_factory=list)
 
     def __len__(self):
         return len(self.children)
+
+    def __contains__(self, item):
+        return item in self.children or item in self.lights
+
+    @classmethod
+    def default(cls) -> World:
+        lights = [Light(Tuple.point(-10, 10, -10), Color(1, 1, 1))]
+        children = [
+            Sphere(
+                material=Material(
+                    color=Color(0.8, 1.0, 0.6),
+                    diffuse=0.7,
+                    specular=0.2
+                )
+            ),
+            Sphere(
+                transform=Matrix.identity.scale(0.5, 0.5, 0.5)
+            )
+        ]
+        return World(children, lights)
--- tests/test_world.py
+++ tests/test_world.py
@@ -1,7 +1,33 @@
-from tracer import World
+from tracer import (
+    Color,
+    Light,
+    Material,
+    point,
+    Sphere,
+    transforms,
+    World,
+)
 
 
 def test_world():
     world = World()
     assert len(world) == 0
     assert len(world.lights) == 0
+
+
+def test_default_world():
+    light = Light(point(-10, 10, -10), Color(1, 1, 1))
+    sphere_1 = Sphere(
+        material=Material(
+            color=Color(0.8, 1.0, 0.6),
+            diffuse=0.7,
+            specular=0.2
+        )
+    )
+    sphere_2 = Sphere(
+        transform=transforms.scaling(0.5, 0.5, 0.5)
+    )
+    world = World.default()
+    assert world.lights[0] == light
+    assert sphere_1 in world
+    assert sphere_2 in world
